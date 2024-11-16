b0782527d9ec32421c3997de2ea340641e9ebc4d
Tung Nguyen <tungnv.codetot@gmail.com>
2024-11-16 23:41:30 +0700

    Fix Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -17,8 +17,8 @@ RUN apt-get update && \
 # RUN wget https://mirror.cs.uchicago.edu/google-chrome/pool/main/g/google-chrome-stable/google-chrome-stable_114.0.5735.90-1_amd64.deb -O /tmp/chrome.deb
 # RUN dpkg -i /tmp/chrome.deb || apt-get install -f -y
 
-RUN wget http://security.ubuntu.com/ubuntu/pool/universe/c/chromium-browser/chromium-browser_112.0.5615.49-0ubuntu0.18.04.1_amd64.deb -O /tmp/chromium.deb
-RUN dpkg -i /tmp/chromium.deb || apt-get install -f -y
+# RUN wget http://security.ubuntu.com/ubuntu/pool/universe/c/chromium-browser/chromium-browser_112.0.5615.49-0ubuntu0.18.04.1_amd64.deb -O /tmp/chromium.deb
+# RUN dpkg -i /tmp/chromium.deb || apt-get install -f -y
 
 # RUN wget https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip -O /tmp/driver.zip && \
 #     unzip /tmp/driver.zip -d /usr/local/bin/ && \
@@ -32,10 +32,10 @@ RUN pip install --no-cache-dir -r requirements.txt
 # COPY . /app
 
 # ENV PATH="/usr/lib/chromium-browser/:${PATH}"
-ENV CHROME_BIN="/usr/bin/chromium"
+# ENV CHROME_BIN="/usr/bin/chromium"
 # ENV CHROME_BIN=/usr/bin/google-chrome-stable
 # ENV CHROMEDRIVER_PATH=/usr/local/bin/chromedriver
-ENV CHROMEDRIVER_PATH=/usr/lib/chromium-browser/chromedriver
+# ENV CHROMEDRIVER_PATH=/usr/lib/chromium-browser/chromedriver
 
 # RUN pip install gunicorn
 
--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import time
 
 app = Flask(__name__)
 
-# Định nghĩa route để nhận cookie và user-agent
 @app.route('/use-cookie', methods=['POST'])
 def use_cookie():
     cookies = request.json.get("cookies", "")
@@ -17,33 +16,33 @@ def use_cookie():
     if not cookies or not user_agent or not imei:
         return jsonify({"error": "Cookies, User-Agent and IMEI is required!"}), 400
 
-    # Thiết lập các options cho Selenium
     options = Options()
     options.add_argument("--headless")  # Headless mode
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
     options.add_argument(f"user-agent={user_agent}")
+
+    chromedriver_path = "/usr/local/bin/chromedriver" 
+    service = Service(executable_path=chromedriver_path)
+
     
-    service = Service(ChromeDriverManager().install())
+    service = Service(/usr/bin/chromedriver)
     driver = webdriver.Chrome(service=service, options=options)
     
     driver.get('https://chat.zalo.me/')
     
-    # Cài đặt cookie
     cookie_dict = cookies.split(';')
     for cookie in cookie_dict:
         cookie_name_value = cookie.strip().split('=')
         if len(cookie_name_value) == 2:
             driver.add_cookie({"name": cookie_name_value[0], "value": cookie_name_value[1]})
 
-    # Thiết lập IMEI vào localStorage
     driver.execute_script(f"localStorage.setItem('z_uuid', '{imei}');")
 
     driver.refresh()
     time.sleep(8)
 
-    # Chụp ảnh màn hình dưới dạng base64
     screenshot = driver.get_screenshot_as_base64()
     
     driver.quit()
@@ -53,27 +52,40 @@ def use_cookie():
         "screenshot": screenshot
     })
 
-# Route để lấy trang web và trả về HTML
 @app.route('/', methods=['GET'])
 def index():
-    url = request.args.get('url', 'https://www.google.com')  # URL mặc định là Google nếu không có tham số
+    url = request.args.get('url', 'https://www.google.com')
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
 
-    service = Service(ChromeDriverManager().install())
+    service = Service('/usr/bin/chromedriver')
     driver = webdriver.Chrome(service=service, options=options)
     
     driver.get(url)
     
-    # Lấy HTML của trang
-    page_html = driver.page_source
+    # page_html = driver.page_source
+    time.sleep(5)
+
+    screenshot = driver.get_screenshot_as_base64()
+
+     html = f"""<!DOCTYPE html>
+        <html>
+        <head>
+            <title>Screenshot</title>
+        </head>
+        <body>
+            <img src="data:image/png;base64,{screenshot}" alt="Screenshot">
+        </body>
+        </html>"""
     
     driver.quit()
+
+    return html
     
-    return page_html
+    # return page_html
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
